d1f83bead02bfcd1ed5550d6871c4234877ebf6c
bluebirdboss <70698171+bluebirdboss@users.noreply.github.com>
2024-10-06 23:17:12 +1100

    Update smbserver.sh

--- smb/smbserver.sh
+++ smb/smbserver.sh
@@ -8,8 +8,8 @@ sudo wget https://raw.githubusercontent.com/bluebirdboss/pi_scripts/main/smb/smb
 
 echo Manually add user to smb service 
 
-echo sudo smbpasswd -a pi
+sudo smbpasswd -a pi
 
 echo after user created restart service
 
-echo sudo systemctl restart smbd
+sudo systemctl restart smbd
